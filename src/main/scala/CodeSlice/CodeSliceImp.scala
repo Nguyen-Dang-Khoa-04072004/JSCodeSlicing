e2574d1d07a2b8bdb3130f38c7a8a1cf4de90ca5
RavenTheshadow <hogiathang2@gmail.com>
2025-10-22 09:19:38 +0700

    Remove Wrapper

--- src/main/scala/CodeSlice/CodeSliceImp.scala
+++ src/main/scala/CodeSlice/CodeSliceImp.scala
@@ -1,67 +1,66 @@
 package CodeSlice
 
 import io.joern.jssrc2cpg.{Config, JsSrc2Cpg}
-import JoernWrapper.{JoernService, JoernServiceImpl}
-import io.shiftleft.codepropertygraph.Cpg
 import scala.util.{Success, Failure}
 import Type.Source.SourceGroups
 import Type.Sink.SinkGroups
 import Type.{CallType, CodeType, RegexType}
 
-class CodeSliceImp(inputDir : String, outputDir: String) extends CodeSlice {
+class CodeSliceImp(inputDir: String, outputDir: String) extends CodeSlice {
 
-  private val joernService: JoernService = JoernServiceImpl
-  val cpg: Cpg = joernService.createCpg(inputPath = inputDir, enableTsTypes = false).getOrElse(
-    throw new RuntimeException("Fail to get CPG")
-  )
-  
+  println(inputDir)
+  println(outputDir)
+
   // TODO: thang
   override def getSinkMethodGroup: SinkMethodGroup = {
     val sinkMethodGroup = new SinkMethodGroup()
-    
+
     for (sink <- SinkGroups.getAllSinks) {
       sink match {
         case CallType(value) =>
-          // TODO: Handle CallType sinks - process function calls
-          // Example: find calls to eval, exec, fs.writeFile, etc.
-          
+        // TODO: Handle CallType sinks - process function calls
+        // Example: find calls to eval, exec, fs.writeFile, etc.
+
         case CodeType(value) =>
-          // TODO: Handle CodeType sinks - process code/variable references
-          // Example: find references to innerHTML, location.href, etc.
-          
+        // TODO: Handle CodeType sinks - process code/variable references
+        // Example: find references to innerHTML, location.href, etc.
+
         case RegexType(value) =>
-          // TODO: Handle RegexType sinks - process pattern matches
-          // Example: find patterns matching dangerous operations
+        // TODO: Handle RegexType sinks - process pattern matches
+        // Example: find patterns matching dangerous operations
       }
     }
-    
+
     sinkMethodGroup
   }
 
   // TODO: thang
   override def getSourceMethodGroup: SourceMethodGroup = {
     val sourceMethodGroup = new SourceMethodGroup()
-    
+
     for (source <- SourceGroups.getAllSources) {
       source match {
         case CallType(value) =>
-          // TODO: Handle CallType sources - process function calls
-          // Example: find calls to eval, exec, fs.writeFile, etc.
-          
+        // TODO: Handle CallType sources - process function calls
+        // Example: find calls to eval, exec, fs.writeFile, etc.
+
         case CodeType(value) =>
-          // TODO: Handle CodeType sources - process code/variable references
-          // Example: find references to innerHTML, location.href, etc.
-          
+        // TODO: Handle CodeType sources - process code/variable references
+        // Example: find references to innerHTML, location.href, etc.
+
         case RegexType(value) =>
-          // TODO: Handle RegexType sources - process pattern matches
-          // Example: find patterns matching dangerous operations
+        // TODO: Handle RegexType sources - process pattern matches
+        // Example: find patterns matching dangerous operations
       }
     }
-    
+
     sourceMethodGroup
   }
   // TODO: khoa
-  override def getPathLine(sourceMethodGroup: SourceMethodGroup, sinkMethodGroup: SinkMethodGroup): PathLine = ???
+  override def getPathLine(
+      sourceMethodGroup: SourceMethodGroup,
+      sinkMethodGroup: SinkMethodGroup
+  ): PathLine = ???
   // TODO: thang
   override def extractCode(pathLine: PathLine): String = ???
 }
--- src/main/scala/FileProcessor/IOFileProcessor.scala
+++ src/main/scala/FileProcessor/IOFileProcessor.scala
@@ -1,58 +1,71 @@
 package FileProcessor
 
 trait IOFileProcessor {
+
   /** List all package directories in a specific directory
-   *
-   * @param directoryPath
-   * Path to the directory to scan
-   * @return
-   * Sequence of package directory paths
-   */
+    *
+    * @param directoryPath
+    *   Path to the directory to scan
+    * @return
+    *   Sequence of package directory paths
+    */
   def listPackageDirectories(directoryPath: String): Seq[String]
 
   /** Load checkpoint file to get already processed packages
-   *
-   * @param checkpointFilePath
-   * Path to the checkpoint file
-   * @return
-   * Set of processed package names
-   */
+    *
+    * @param checkpointFilePath
+    *   Path to the checkpoint file
+    * @return
+    *   Set of processed package names
+    */
   def loadCheckpoint(checkpointFilePath: String): Set[String]
 
   /** Save a package name to the checkpoint file
-   *
-   * @param packageName
-   * Name of the package to save
-   * @return
-   * Boolean indicating success/failure
-   */
-  def saveCheckpoint(checkpointFilePath:String, packageName: String): Boolean
+    *
+    * @param packageName
+    *   Name of the package to save
+    * @return
+    *   Boolean indicating success/failure
+    */
+  def saveCheckpoint(checkpointFilePath: String, packageName: String): Boolean
 
   /** Check if a package has been processed (exists in checkpoint)
-   *
-   * @param packageName
-   * Name of the package to check
-   * @param processedPackages
-   * Set of already processed packages
-   * @return
-   * Boolean indicating if package was already processed
-   */
-  def isPackageProcessed(packageName: String, processedPackages: Set[String]): Boolean
+    *
+    * @param packageName
+    *   Name of the package to check
+    * @param processedPackages
+    *   Set of already processed packages
+    * @return
+    *   Boolean indicating if package was already processed
+    */
+  def isPackageProcessed(
+      packageName: String,
+      processedPackages: Set[String]
+  ): Boolean
 
   /** Save output packages (CPG results)
-   * @param outputFilePath
-   * directory to save output packages
-   * @param content
-   * content to save in the output package
-   */
+    * @param outputFilePath
+    *   directory to save output packages
+    * @param content
+    *   content to save in the output package
+    */
   def saveOutputPackage(outputFilePath: String, content: String): Boolean
 
   /** Get package name from directory path
-   *
-   * @param packageDir
-   * Directory of the package
-   * @return
-   * Package name
-   */
+    *
+    * @param packageDir
+    *   Directory of the package
+    * @return
+    *   Package name
+    */
   def getPackageName(packageDir: String): String
+
+  /** Create output directory if it does not exist
+    *
+    * @param outputDir
+    *   Path to the output directory
+    * @return
+    *   Output directory file path
+    */
+  def createOutputDirectoryIfNotExists(outputDir: String): String
 }
