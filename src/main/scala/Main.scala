fff7b626802f95714054de34781d546220b11f63
RavenTheshadow <hogiathang2@gmail.com>
2025-10-22 09:19:38 +0700

    Remove Wrapper

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,24 @@ import FileProcessor.{IOFileProcessorImpl, IOFileProcessor}
 
 object Main {
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        val ioFileProcessor: IOFileProcessor = IOFileProcessorImpl
-        
-        for (packagePath: String <- ioFileProcessor.listPackageDirectories("src/main/resources/input/")) {
-            val codeSlice: CodeSlice = new CodeSliceImp(packagePath, "src/main/resources/output/")
-            // Execute the slicing process (when methods are implemented)
-            // codeSlice.asInstanceOf[CodeSliceImp].executeSlicing()
-        }
+    val ioFileProcessor: IOFileProcessor = IOFileProcessorImpl
+
+    for (
+      packagePath: String <- ioFileProcessor.listPackageDirectories(
+        "src/main/resources/input/"
+      )
+    ) {
+      val codeSlice: CodeSlice = new CodeSliceImp(
+        packagePath,
+        ioFileProcessor.createOutputDirectoryIfNotExists(
+          "src/main/resources/output/" + ioFileProcessor.getPackageName(
+            packagePath
+          ) + "/"
+        )
+      )
     }
+  }
 
 }
