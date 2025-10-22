e7d6b3e2b6841f74f08f344df7db1c583ecd9bd8
RavenTheshadow <hogiathang2@gmail.com>
2025-10-22 08:21:44 +0700

    Add Wrapper

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ ThisBuild / scalaVersion := "3.6.4"
 libraryDependencies ++= Seq(
   "io.joern" %% "joern-cli" % "4.0.436",
   "io.joern" %% "jssrc2cpg" % "4.0.436",
-  "com.google.guava" % "guava" % "33.0.0-jre"
+  "io.joern" %% "x2cpg" % "4.0.436",
+  "com.google.guava" % "guava" % "33.0.0-jre",
+  "org.slf4j" % "slf4j-simple" % "2.0.16"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,19 +6,11 @@ object Main {
     def main(args: Array[String]): Unit = {
 
         val ioFileProcessor: IOFileProcessor = IOFileProcessorImpl
-
+        
         for (packagePath: String <- ioFileProcessor.listPackageDirectories("src/main/resources/input/")) {
-
-            val codeSlice: CodeSlice = new CodeSliceImp(packagePath)
-
-            val sourceMethodGroup    = codeSlice.getSourceMethodGroup
-            val sinkMethodGroup      = codeSlice.getSinkMethodGroup
-            val pathLine             = codeSlice.getPathLine(sourceMethodGroup,sinkMethodGroup)
-            val codeSliced           = codeSlice.extractCode(pathLine)
-
-            ioFileProcessor.saveOutputPackage("src/main/resources/output/" + ioFileProcessor.getPackageName(packagePath) + "_sliced.txt", codeSliced)
-            val checkpoint = ioFileProcessor.saveCheckpoint("src/main/resources/checkpoint/checkpoint.txt", ioFileProcessor.getPackageName(packagePath))
-
+            val codeSlice: CodeSlice = new CodeSliceImp(packagePath, "src/main/resources/output/")
+            // Execute the slicing process (when methods are implemented)
+            // codeSlice.asInstanceOf[CodeSliceImp].executeSlicing()
         }
     }
 
